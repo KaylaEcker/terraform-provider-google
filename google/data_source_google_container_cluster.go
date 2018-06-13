966bf2c458630e6e352d607ad1b41f53db94dc36
Kayla Ecker <kaylajayecker@gmail.com>
2018-06-12 21:51:26 -0700

    added labels to google container cluster

--- google/data_source_google_container_cluster.go
+++ google/data_source_google_container_cluster.go
@@ -17,6 +17,18 @@ func dataSourceGoogleContainerCluster() *schema.Resource {
 	return &schema.Resource{
 		Read:   datasourceContainerClusterRead,
 		Schema: dsSchema,
+		
+		"labels": &schema.Schema{
+		Type:     schema.TypeMap,
+		Optional: true,
+		Elem:     &schema.Schema{Type: schema.TypeString},
+		Set:      schema.HashString,
+		},
+
+		"label_fingerprint": &schema.Schema{
+		Type:     schema.TypeString,
+		Computed: true,
+		},
 	}
 }
 
@@ -24,6 +36,8 @@ func datasourceContainerClusterRead(d *schema.ResourceData, meta interface{}) er
 	clusterName := d.Get("name").(string)
 
 	d.SetId(clusterName)
+	d.Set("labels", image.Labels)
+	d.Set("label_fingerprint", image.LabelFingerprint)
 
 	return resourceContainerClusterRead(d, meta)
 }
